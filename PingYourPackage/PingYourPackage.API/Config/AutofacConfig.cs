6eac6be131c2d0a8867993125ff422d65adcefcc
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-21 11:13:54 +0530

    created API Project

---
+++ PingYourPackage/PingYourPackage.API/Config/AutofacConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace PingYourPackage.API.Config
+{
+    public class AutofacWebAPI
+    {
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config,
+            RegisterServices(new ContainerBuilder()));
+        }
+        public static void Initialize(
+        HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver =
+            new AutofacWebApiDependencyResolver(container);
+        }
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // registration goes here
+            return builder.Build();
+        }
+    }
+}
