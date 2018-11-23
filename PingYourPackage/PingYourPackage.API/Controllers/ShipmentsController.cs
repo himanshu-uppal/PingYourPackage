81bb5017535ec529aad387c656079b5748f2d23b
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-11-23 12:59:48 +0530

    implemented basic  shipment controller

---
+++ PingYourPackage/PingYourPackage.API/Controllers/ShipmentsController.cs
@@ -0,0 +1,29 @@
+﻿using PingYourPackage.API.Model.Dtos;
+using PingYourPackage.API.Model.RequestCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * GET /api/shipments
+ * GET /api/shipments/{key}
+ * POST /api/shipments
+ * PUT /api/shipments/{key}
+ */
+namespace PingYourPackage.API.Controllers
+{
+    public class ShipmentsController
+    {
+        public PaginatedDto<ShipmentDto>
+ GetShipments(PaginatedRequestCommand cmd)
+        {
+            var shipments = _shipmentService
+            .GetShipments(cmd.Page, cmd.Take);
+            return shipments.ToPaginatedDto(
+            shipments.Select(sh => sh.ToShipmentDto()));
+        }
+    }
+}
